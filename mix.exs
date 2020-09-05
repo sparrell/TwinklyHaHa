ff84e17f57ac5860ca2c748392698ac30adb35c5
Duncan Sparrell <duncan@sfractal.com>
2020-09-05 08:50:30 -0400

    trying to understand mix version

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule Twinklyhaha.MixProject do
   def project do
     [
       app: :twinklyhaha,
-      version: "0.1.0",
+      version: "0.5.2",
       elixir: "~> 1.7",
       elixirc_paths: elixirc_paths(Mix.env()),
       compilers: [:phoenix, :gettext] ++ Mix.compilers(),
