31f2b2ef3ac81cfe0d7d1d30f6d9cd215861991c
WanjikuMac <edwardscaroline32@gmail.com>
2020-11-12 08:54:33 +0300

    update mix dependancies

--- mix.exs
+++ mix.exs
@@ -5,7 +5,7 @@ defmodule Twinklyhaha.MixProject do
     [
       app: :twinklyhaha,
       version: "0.1.0",
-      elixir: "~> 1.7",
+      elixir: "~> 1.11.2",
       elixirc_paths: elixirc_paths(Mix.env()),
       compilers: [:phoenix, :gettext] ++ Mix.compilers(),
       start_permanent: Mix.env() == :prod,
@@ -33,7 +33,7 @@ defmodule Twinklyhaha.MixProject do
   # Type `mix help deps` for examples and options.
   defp deps do
     [
-      {:phoenix, "~> 1.5.4"},
+      {:phoenix, "~> 1.5.6"},
       {:phoenix_ecto, "~> 4.1"},
       {:ecto_sql, "~> 3.4"},
       {:postgrex, ">= 0.0.0"},
